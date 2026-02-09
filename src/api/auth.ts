da4693b0557e31ea9b9686674be4c08466bf5c20
Aisha AbuJeib <s12113150@stu.najah.edu>
2026-02-09 19:48:33 +0200

    Break getAPIKey to verify CI fails

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,10 +1,8 @@
 import { IncomingHttpHeaders } from "http";
 
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
-  const authHeader = headers["authorization"];
-  if (!authHeader) {
-    return null;
-  }
+  return null;
+}
 
   const splitAuth = authHeader.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
