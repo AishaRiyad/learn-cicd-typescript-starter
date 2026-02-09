61013c9dc4639de608e2360bc2ad347bd6ce533c
Aisha AbuJeib <s12113150@stu.najah.edu>
2026-02-09 21:52:31 +0200

    Add eslint.config.js for ESLint v9

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  { ignores: ["dist/**", "node_modules/**"] },
+
+  {
+    ...js.configs.recommended,
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^(req|res|next|user|_)$" },
+      ],
+    },
+  },
+];
+
